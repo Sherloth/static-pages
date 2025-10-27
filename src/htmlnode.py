836b95d9fd1ce6778ffbec4e196ed7d1a1110790
Sherloth <dl.kmiecik@gmail.com>
2025-10-27 20:04:58 +0000

    LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,4 +20,16 @@ class HTMLNode:
             f"children={repr(self.children)}, "
             f"props={repr(self.props)})"
         )
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+        
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("No Value!")
+        if self.tag == None:
+            return self.value
+        props_str = self.props_to_html()
+        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
 
