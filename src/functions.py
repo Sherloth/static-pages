71e43fabd1b341a2e246c997a8563d21ec668169
Sherloth <dl.kmiecik@gmail.com>
2025-11-08 21:52:11 +0000

    regex from nodes

--- src/functions.py
+++ src/functions.py
@@ -25,3 +25,55 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\]]+)\]\(([^)]+)\)", text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        matches = extract_markdown_images(text)
+
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        parts = re.split(r"!\[[^\]]+\]\([^)]+\)", text)
+
+        for i, part in enumerate(parts):
+            if part:
+                new_nodes.append(TextNode(part, TextType.TEXT))
+            if i < len(matches):
+                alt, url = matches[i]
+                new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        matches = extract_markdown_links(text)
+
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        parts = re.split(r"(?<!!)\[[^\]]+\]\([^)]+\)", text)
+
+        for i, part in enumerate(parts):
+            if part:
+                new_nodes.append(TextNode(part, TextType.TEXT))
+            if i < len(matches):
+                anchor, url = matches[i]
+                new_nodes.append(TextNode(anchor, TextType.LINK, url))
+
+    return new_nodes
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,7 +6,13 @@ import unittest
 sys.path.append(os.path.dirname(__file__))
 
 from textnode import TextNode, TextType
-from functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from functions import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+)
 
 
 class TestTextNode(unittest.TestCase):
@@ -16,7 +22,7 @@ class TestTextNode(unittest.TestCase):
 
         node3 = TextNode("This is a different text node", TextType.LINK, "https://example.com")
         node4 = TextNode("This is a different text node", TextType.LINK, "https://example.com")
-        node5 = TextNode("This is a different text node", TextType.LINK, "https://other.com")  # no None
+        node5 = TextNode("This is a different text node", TextType.LINK, "https://other.com")
 
         self.assertEqual(node, node2)
         self.assertEqual(node3, node4)
@@ -129,7 +135,8 @@ class TestSplitNodesDelimiter(unittest.TestCase):
                 TextNode("!", TextType.TEXT),
             ],
         )
-    
+
+    # --- Markdown regex helpers ---
     def test_regex_markdown(self):
         a = "I'm a little ![teapot](https://example.com/teapot.png)"
         b = "This has [lane](https://example.com/lane) and [hunter](https://example.org/hunter)"
@@ -141,5 +148,76 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             extract_markdown_links(b),
             [("lane", "https://example.com/lane"), ("hunter", "https://example.org/hunter")]
         )
+
+    def test_images_and_links_together(self):
+        text = "Here is ![img](https://example.com/img.png) and [link](https://example.com)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("img", "https://example.com/img.png")]
+        )
+        self.assertEqual(
+            extract_markdown_links(text),
+            [("link", "https://example.com")]
+        )
+
+
+class TestSplitNodesImageAndLink(unittest.TestCase):
+    def test_split_nodes_link_basic(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        nodes = split_nodes_link([node])
+
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+            ],
+        )
+
+    def test_split_nodes_link_no_links(self):
+        node = TextNode("No links here, just vibes.", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(nodes, [node])
+
+    def test_split_nodes_link_preserves_non_text_nodes(self):
+        # Should not touch existing LINK nodes
+        link_node = TextNode("already a link", TextType.LINK, "https://example.com")
+        nodes = split_nodes_link([link_node])
+        self.assertEqual(nodes, [link_node])
+
+    def test_split_nodes_image_basic(self):
+        node = TextNode(
+            "Here is an image ![alt](https://example.com/img.png) and another ![second](https://example.com/second.png).",
+            TextType.TEXT,
+        )
+        nodes = split_nodes_image([node])
+
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("Here is an image ", TextType.TEXT),
+                TextNode("alt", TextType.IMAGE, "https://example.com/img.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second", TextType.IMAGE, "https://example.com/second.png"),
+                TextNode(".", TextType.TEXT),
+            ],
+        )
+
+    def test_split_nodes_image_no_images(self):
+        node = TextNode("Nothing to see here, no images.", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        self.assertEqual(nodes, [node])
+
+    def test_split_nodes_image_preserves_non_text_nodes(self):
+        img_node = TextNode("logo", TextType.IMAGE, "https://example.com/logo.png")
+        nodes = split_nodes_image([img_node])
+        self.assertEqual(nodes, [img_node])
+
+
 if __name__ == "__main__":
     unittest.main()
